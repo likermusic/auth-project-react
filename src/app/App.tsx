f9d8b2c4fb9caaaeb6cfdb2d4f56c7c4f3180c35
Sergey Timofeev <drumliker@yandex.ru>
2025-02-26 22:05:06 +0300

    Config app and install libs

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/shared/ui/button";
+
+function App() {
+  return (
+    <Button className="m-4 cursor-pointer" size={"lg"}>
+      BUtton
+    </Button>
+  );
+}
+
+export default App;
